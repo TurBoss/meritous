65e9ff44963cfa3979c75a20c7a740911293329a
TurBoss <j.l.toledano.l@gmail.com>
2019-02-25 06:49:29 +0100

    audio settings

--- src/audio.c
+++ src/audio.c
@@ -52,7 +52,7 @@ Mix_Chunk *c_sample[16] = {NULL};
 
 void InitAudio()
 {
-	Mix_OpenAudio(48000, AUDIO_S16, 2, 512);
+	Mix_OpenAudio(22000, AUDIO_S16, 2, 512);
 	Mix_VolumeMusic(112);
 	Mix_AllocateChannels(16);
 }
@@ -70,7 +70,7 @@ void CircuitHum()
 {
 	int hum_vol = 0;
 	static int hum_play = 0;
-	
+
 	if (magic_circuit == 0) {
 		hum_vol = 0;
 		if (hum_play != 0) {
@@ -81,7 +81,7 @@ void CircuitHum()
 	if (magic_circuit > 0) {
 		hum_vol = (circuit_size * 24 / 3000)+12;
 		hum_vol = (magic_circuit * 96 / circuit_size) * hum_vol / 36 + 32;
-		
+
 		if (hum_play != 1) {
 			if (hum_play == 2) {
 				Mix_HaltChannel(0);
@@ -104,7 +104,7 @@ void CircuitHum()
 			hum_play = 2;
 		}
 	}
-	
+
 	Mix_Volume(0, hum_vol);
 }
 
@@ -116,9 +116,9 @@ void TitleScreenMusic()
 		Mix_FreeMusic(bgm_music);
 		bgm_music = NULL;
 	}
-	
+
 	bgm_music = Mix_LoadMUS(tracks[new_track]);
-	Mix_PlayMusic(bgm_music, -1);	
+	Mix_PlayMusic(bgm_music, -1);
 	bgm_track = new_track;
 }
 
@@ -126,11 +126,11 @@ void BackgroundMusic()
 {
 	int new_track = -1;
 	Mix_VolumeMusic(96);
-	
+
 	if (rooms[player_room].s_dist <= 15) {
 			new_track = 0;
 	}
-	
+
 	if (bgm_track == 1) {
 		if ((rooms[player_room].s_dist <= 30) && (rooms[player_room].s_dist >= 10)) {
 			new_track = 1;
@@ -141,7 +141,7 @@ void BackgroundMusic()
 			new_track = 2;
 		}
 	}
-	
+
 	if (new_track == -1) {
 		if ((rooms[player_room].s_dist <= 30) && (rooms[player_room].s_dist >= 16)) {
 			new_track = 1;
@@ -153,15 +153,15 @@ void BackgroundMusic()
 			new_track = 3;
 		}
 	}
-	
+
 	if (rooms[player_room].room_type == 3) {
 		new_track = 4;
 	}
-	
+
 	if (artifacts[11]) {
 		new_track = 6;
 	}
-	
+
 	if (rooms[player_room].room_type == 2) {
 		if (boss_fight_mode == 2) {
 			if ( (current_boss == 3) && (player_shield == 30) ) {
@@ -178,11 +178,11 @@ void BackgroundMusic()
 			new_track = -1;
 		}
 	}
-	
+
 	if ( (player_room == 0) && (current_boss == 3) && (boss_fight_mode >= 3) ) {
 		new_track = 11;
 	}
-	
+
 	if (new_track == bgm_track) return;
 
 	if (bgm_track != -1) {
@@ -200,7 +200,7 @@ void BackgroundMusic()
 int SND_GetChannel()
 {
 	int i;
-	
+
 	for (i = 8; i < 16; i++) {
 		if (Mix_Playing(i) == 0) {
 			if (c_sample[i] != NULL) {
@@ -216,7 +216,7 @@ int SND_GetChannel()
 void SND_Play(char *filename, int vol)
 {
 	int ch;
-		
+
 	ch = SND_GetChannel();
 	if (ch != -1) {
 		c_sample[ch] = Mix_LoadWAV(filename);
@@ -229,7 +229,7 @@ void SND_Pos(char *filename, int vol, int dist)
 {
 	int real_vol;
 	if (dist > 1600) return;
-	
+
 	real_vol = vol * (40 - (sqrt(dist))) / 40;
 	SND_Play(filename, real_vol);
 }
@@ -238,4 +238,3 @@ void SND_CircuitRelease(int str)
 {
 	SND_Play("dat/a/circuitrelease.wav", sqrt(str * 2 + (str * 5300 / circuit_size) + (str > 100 ? 5300 : str*53)));
 }
-
