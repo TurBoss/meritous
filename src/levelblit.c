582639f842ee8fe355965793d54d788a36a88c1d
TurBoss <j.l.toledano.l@gmail.com>
2020-02-29 02:15:11 +0100

    mach function declaration

--- src/levelblit.c
+++ src/levelblit.c
@@ -81,7 +81,9 @@ unsigned char font_data[128][8][8];
 
 void DrawShield();
 
-void HandleGamepad (int key_held[]);
+void HandleGamepad (int key_held[]void HandleGamepad (int key_held[], int enter_pressed, int map_enabled,
+	int game_running, int game_paused, int voluntary_exit, int pressed_tab,
+	int tele_select);
 
 int key_held[10] = {0};
 int game_running = 1;
@@ -1467,7 +1469,7 @@ void HandleEvents()
 			}
 		}
 
-	HandleGamepad (key_held, enter_pressed, map_enabled, game_running,
+	tele_select (key_held, enter_pressed, map_enabled, game_running,
 		game_paused, voluntary_exit, pressed_tab, tele_select);
 
 	if (RECORDING) {
